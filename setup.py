b471ee14e8597ec6561bdbdf26b63baf97f6bc54
Simon Baatz <gmbnomis@gmail.com>
2018-10-18 21:22:08 +0200

    Adapt to pulp plugin 0.1.0b11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    'pulpcore-plugin>=0.1.0b5',
+    'pulpcore-plugin>=0.1.0b11',
 ]
 
 with open('README.rst') as f:
@@ -11,7 +11,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-cookbook',
-    version='0.0.2a2',
+    version='0.0.3a1',
     description='Cookbook plugin for the Pulp Project',
     long_description=long_description,
     author='Simon Baatz',

4fcdb4af2eae2492b0bc8b7307ebe247b40893c6
Simon Baatz <gmbnomis@gmail.com>
2019-05-09 22:41:09 +0200

    Add the possibility to include pulp-smash PRs

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,26 +13,37 @@ cd ..
 if [ -z "$TRAVIS_TAG" ]; then
   export PULP_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulpcore\/pull\/(\d+)' | awk -F'/' '{print $7}')
   export PULP_PLUGIN_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulpcore-plugin\/pull\/(\d+)' | awk -F'/' '{print $7}')
+  export PULP_SMASH_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/PulpQE\/pulp-smash\/pull\/(\d+)' | awk -F'/' '{print $7}')
 
   git clone --depth 1 https://github.com/pulp/pulpcore.git
   if [ -n "$PULP_PR_NUMBER" ]; then
-    pushd pulpcore
+    cd pulpcore
     git fetch origin +refs/pull/$PULP_PR_NUMBER/merge
     git checkout FETCH_HEAD
-    popd
+    cd ..
   fi
 
   pip install -e ./pulpcore[postgres]
 
   git clone --depth 1 https://github.com/pulp/pulpcore-plugin.git
   if [ -n "$PULP_PLUGIN_PR_NUMBER" ]; then
-    pushd pulpcore-plugin
+    cd pulpcore-plugin
     git fetch origin +refs/pull/$PULP_PLUGIN_PR_NUMBER/merge
     git checkout FETCH_HEAD
-    popd
+    cd ..
   fi
 
   pip install -e ./pulpcore-plugin
+
+  if [ -n "$PULP_SMASH_PR_NUMBER" ]; then
+    pip uninstall -y pulp-smash
+    git clone https://github.com/PulpQE/pulp-smash.git
+    cd pulp-smash
+    git fetch origin +refs/pull/$PULP_SMASH_PR_NUMBER/merge
+    git checkout FETCH_HEAD
+    pip install -e .
+    cd ..
+  fi
 fi
 
 cd pulp_cookbook

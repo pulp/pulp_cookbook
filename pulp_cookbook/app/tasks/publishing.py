c01ea4d6445dfceb03ef34cf9492c937a27bcdb8
Simon Baatz <gmbnomis@gmail.com>
2020-01-18 22:09:52 +0100

    Adapt batching publication to upstream content_batch_qs() generator
    See upstream issue https://pulp.plan.io/issues/6024.

--- pulp_cookbook/app/tasks/publishing.py
+++ pulp_cookbook/app/tasks/publishing.py
@@ -52,76 +52,64 @@ def publish(repository_version_pk):
     )
 
     with WorkingDirectory():
-        with CookbookPublication.create(repository_version) as publication:
-            universe = Universe("__universe__")
-            universe.write(populate(publication))
-            PublishedMetadata.create_from_file(
-                relative_path=os.path.basename(universe.relative_path),
-                publication=publication,
-                file=File(open(universe.relative_path, "rb")),
-            )
+        with ProgressReport(
+            message="Publishing Content", code="publishing.content"
+        ) as progress_report:
+            with CookbookPublication.create(repository_version) as publication:
+                universe = Universe("__universe__")
+                universe.write(populate(publication, progress_report=progress_report))
+                PublishedMetadata.create_from_file(
+                    relative_path=os.path.basename(universe.relative_path),
+                    publication=publication,
+                    file=File(open(universe.relative_path, "rb")),
+                )
 
     log.info(_("Publication: %(publication)s created"), {"publication": publication.pk})
 
 
-def batch_qs(qs, batch_size=1000):
-    """
-    Returns a queryset batch in the given queryset.
-
-    Usage:
-        # Make sure to order your querset
-        article_qs = Article.objects.order_by('id')
-        for qs in batch_qs(article_qs):
-            for article in qs:
-                print article.body
-    """
-    total = qs.count()
-    for start in range(0, total, batch_size):
-        end = min(start + batch_size, total)
-        yield qs[start:end]
-
-
-def populate(publication, batch_size=BATCH_SIZE):
+def populate(publication, progress_report=None, batch_size=BATCH_SIZE):
     """
     Populate a publication.
 
     Create published artifacts and yield a Universe Entry for each.
 
     Args:
-        publication (pulpcore.plugin.models.Publication): A Publication to populate.
+        publication (:class:`~pulp_cookbook.models.CookbookPublication`): CookbookPublication
+            to populate.
+        progress_report (:class:`~pulpcore.plugin.models.ProgressReport`): If given, used to
+            report progress
+        batch_size (int): Size of content batches to process at once
 
     Yields:
         Entry: Universe entry for each cookbook
 
     """
-    content_ca_qs = (
-        CookbookPackageContent.objects.filter(pk__in=publication.repository_version.content)
-        .order_by("-pulp_created", "pk")
-        .prefetch_related("contentartifact_set")
+    content_batches = publication.repository_version.content_batch_qs(
+        content_qs=CookbookPackageContent.objects.all(), batch_size=batch_size,
     )
 
-    with ProgressReport(message="Publishing Content", code="publishing.content") as pb:
-        for content_slice_qs in batch_qs(content_ca_qs, batch_size=batch_size):
-            published_artifacts = []
-            for content in content_slice_qs:
-                relative_path = "cookbook_files/{}/{}/".format(
-                    content.name, content.version.replace(".", "_")
+    for content_slice_qs in content_batches:
+        published_artifacts = []
+        for content in content_slice_qs.prefetch_related("contentartifact_set"):
+            relative_path = "cookbook_files/{}/{}/".format(
+                content.name, content.version.replace(".", "_")
+            )
+            for content_artifact in content.contentartifact_set.all():
+                art_path = os.path.join(relative_path, content_artifact.relative_path)
+                published_artifacts.append(
+                    PublishedArtifact(
+                        relative_path=art_path,
+                        publication=publication,
+                        content_artifact=content_artifact,
+                    )
                 )
-                for content_artifact in content.contentartifact_set.all():
-                    art_path = os.path.join(relative_path, content_artifact.relative_path)
-                    published_artifacts.append(
-                        PublishedArtifact(
-                            relative_path=art_path,
-                            publication=publication,
-                            content_artifact=content_artifact,
-                        )
-                    )
-                    entry = Entry(
-                        name=content.name,
-                        version=content.version,
-                        download_url=path_template(art_path),
-                        dependencies=content.dependencies,
-                    )
-                    yield entry
-            PublishedArtifact.objects.bulk_create(published_artifacts)
-            pb.increase_by(len(published_artifacts))
+                entry = Entry(
+                    name=content.name,
+                    version=content.version,
+                    download_url=path_template(art_path),
+                    dependencies=content.dependencies,
+                )
+                yield entry
+        PublishedArtifact.objects.bulk_create(published_artifacts)
+        if progress_report:
+            progress_report.increase_by(len(published_artifacts))
--- pulp_cookbook/tests/unit/test_publishing.py
+++ pulp_cookbook/tests/unit/test_publishing.py
@@ -4,10 +4,15 @@
 
 from unittest.mock import Mock
 
+from django.db import connection, reset_queries
 from django.test import TestCase
+from django.test.utils import override_settings
 
-from pulp_cookbook.app.models import CookbookPackageContent
+from pulpcore.plugin.models import ContentArtifact, PublishedArtifact
+
+from pulp_cookbook.app.models import CookbookPackageContent, CookbookPublication, CookbookRepository
 from pulp_cookbook.app.repo_version_utils import check_repo_version_constraint
+from pulp_cookbook.app.tasks.publishing import populate
 
 
 class CheckRepoVersionConstraintTestCase(TestCase):
@@ -65,3 +70,38 @@ class CheckRepoVersionConstraintTestCase(TestCase):
 
         with self.assertRaisesRegex(ValueError, "would contain multiple versions of cookbooks"):
             check_repo_version_constraint(self.new_version_content())
+
+
+class CheckPublishingPopulate(TestCase):
+    """Verify populate() from the publishing task."""
+
+    def setUp(self):
+        """Setup 20 content units with an associated ContentArtifact."""
+        self.repository = CookbookRepository.objects.create()
+        self.content_count = 20
+        for i in range(self.content_count):
+            c_i = CookbookPackageContent.objects.create(
+                name="cookbook", version=f"{i}.0.0", dependencies={}
+            )
+            ContentArtifact.objects.create(
+                artifact=None, content=c_i, relative_path=c_i.relative_path()
+            )
+        with self.repository.new_version() as self.version1:
+            self.version1.add_content(CookbookPackageContent.objects.all())
+
+    @override_settings(DEBUG=True)  # Set DEBUG to record connection.queries
+    def test_populate_db_ops(self):
+        """Sanity check for the number of DB operations and records created."""
+        publication = CookbookPublication.objects.create(repository_version=self.version1)
+        reset_queries()
+        entries = list(populate(publication, batch_size=int(self.content_count / 2) + 1))
+
+        # 1 query for content count (batch_qs generator) -> 2 batches
+        # 2 for content
+        # 2 for content_artifact prefetch
+        # 2 for bulk create of published artifacts
+        self.assertLessEqual(
+            len(connection.queries), 7, msg=f"More than 7 queries:\n {connection.queries}"
+        )
+        self.assertEqual(len(entries), self.content_count)
+        self.assertEqual(PublishedArtifact.objects.count(), self.content_count)
